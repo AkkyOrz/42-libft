91be462c40b2e954487d6a86c270e89b24777137
Akihide Ito <akky.supernova@gmail.com>
2021-07-13 16:09:51 +0900

    fix substr memory leak

--- ft_substr.c
+++ ft_substr.c
@@ -8,11 +8,11 @@ size_t len)
 		return (NULL);
 	if (ft_strlen(s) - start <= len)
 		len = ft_strlen(s) - start;
+	if ((unsigned int)ft_strlen(s) <= start)
+		return (ft_strdup(""));
 	res = (char *)malloc(sizeof(char) * (len) + 1);
 	if (res == NULL)
 		return (NULL);
-	if ((unsigned int)ft_strlen(s) <= start)
-		return (ft_strdup(""));
 	ft_strlcpy(res, s + start, len + 1);
 	return (res);
 }
