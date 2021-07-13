14aa14f96a12953a37d5ccd86925fc4a8ad42113
Akihide Ito <akky.supernova@gmail.com>
2021-07-13 12:21:24 +0900

    update striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,2 +1,14 @@
 #include "libft.h"
-void ft_striteri(char *s, void (*f)(unsigned int, char*));
+void ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	int i;
+
+	if (s == NULL)
+		return;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		f(i, s);
+		i++;
+	}
+}
