3d21b74645a604dd56bfe1326e0efac389886dd4
Akihide Ito <akky.supernova@gmail.com>
2021-07-16 17:40:46 +0900

    refactor: ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -48,9 +48,7 @@ static int	ft_startwith(char const *str, char const *prefix)
 	while (prefix[i] != '\0')
 	{
 		if (str[i] != prefix[i])
-		{
 			return (-1);
-		}
 		i++;
 	}
 	return (ft_strlen(prefix));
