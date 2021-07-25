b2b74b359b9e15abfb1f5c6d08d404b0e30da592
Akihide Ito <akky.supernova@gmail.com>
2021-07-25 17:31:44 +0900

    fix: unsigned char memory

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,12 +1,12 @@
 #include "libft.h"
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t		i;
-	char		*to;
-	const char	*from;
+	size_t				i;
+	unsigned char		*to;
+	const unsigned char	*from;
 
-	to = (char *)dest;
-	from = (const char *)src;
+	to = (unsigned char *)dest;
+	from = (const unsigned char *)src;
 	if (from == NULL && to == NULL)
 		return (NULL);
 	i = 0;
